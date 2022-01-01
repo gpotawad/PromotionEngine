680b029a0acd65cdd7bf11018bc59be57caa3c14
gpotawad <gpotawad@gmail.com>
2022-01-01 17:07:01 +0530

    project base package and setup

---
+++ src/main/java/com/engine/promotion/MainApplication.java
@@ -0,0 +1,7 @@
+package com.engine.promotion;
+
+public class MainApplication {
+    public static void main(String[] args) {
+        System.out.println("Promotion Engine");
+    }
+}
---
+++ src/test/java/com/engine/promotion/PromotionEngineTest.java
@@ -0,0 +1,18 @@
+package com.engine.promotion;
+
+import org.junit.*;
+
+public class PromotionEngineTest extends Object {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testPromotionEngine() {
+    }
+}
