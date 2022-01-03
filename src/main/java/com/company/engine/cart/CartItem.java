36a3724af94146c441a909edd10f3398adec7151
gpotawad <gpotawad@gmail.com>
2022-01-03 21:44:53 +0530

    CartIem modal to segregate module for testcases

---
+++ src/main/java/com/company/engine/cart/CartItem.java
@@ -0,0 +1,72 @@
+package com.company.engine.cart;
+
+import java.util.Objects;
+
+public class CartItem {
+    char sku;
+    int quantity;
+    int unitPrice;
+    int totalPrice;
+
+    public CartItem(char skuId, int quantity, int unitPrice) {
+        setSku(skuId);
+        setQuantity(quantity);
+        setUnitPrice(unitPrice);
+        setTotalPrice(quantity * unitPrice);
+    }
+
+    public char getSku() {
+        return sku;
+    }
+
+    public void setSku(char sku) {
+        this.sku = sku;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(int unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public int getTotalPrice() {
+        return quantity * unitPrice;
+    }
+
+    public void setTotalPrice(int totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CartItem)) return false;
+        CartItem cartItem = (CartItem) o;
+        return getSku() == cartItem.getSku();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getSku());
+    }
+
+    @Override
+    public String toString() {
+        return "CartItem{" +
+                "sku=" + sku +
+                ", quantity=" + quantity +
+                ", unitPrice=" + unitPrice +
+                ", totalPrice=" + totalPrice +
+                '}';
+    }
+}
